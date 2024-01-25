f1e586570a808c55b5410b65b297e26cae4f8a46
shkslxiv <kimseonho133@gmail.com>
2024-01-25 19:04:21 +0900

    RAAAAAAAAAAHHHHHHHHHHHH RAAAAAAAAAAAHHHHHH WHHHHHA

--- src/main/java/fr/pantheonsorbonne/cri/ArrayList.java
+++ src/main/java/fr/pantheonsorbonne/cri/ArrayList.java
@@ -5,60 +5,92 @@ public class ArrayList {
     String[] data;
     int nb;
 
+
     public ArrayList() {
         this.data = new String[4];
         this.nb = 0;
     }
 
-    boolean ajout(String s) {
+    void ajout(String s) {
         ensureCapacity(this.nb + 1);
         this.data[nb] = s;
-        for (String i : this.data) {
-            nb++;
-            if (i == s) {
-                return true;
-            }
-        }
-        return false;
-
+        nb++;
     }
 
-    public void ensureCapacity(int n) {
-        if (n <= data.length) {
+    boolean add(String element){
+        if
+    }
+
+    void ensureCapacity(int n) {
+        if ( n <= data.length) {
             return;
         }
-        String[] tab2 = new String[2 * data.length];
-        for (int i = 0; i < data.length; i++) {
+        String[] tab2 = new String[2* data.length];
+        for (int i = 0; i < data.length; i ++) {
             tab2[i] = data[i];
         }
         this.data = tab2;
     }
 
-    public String get(int i) {
-        if (i >= 0 && i < data.length) {
-            return this.data[i];
-        }
-        return "erreur";
+    String  get(int i ) {
+
+        return this.data[i];
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (String i : data) {
-            if (i != null)
-                sb.append(i);
+            if(i != null)
+            sb.append(i);
         }
         return sb.toString();
+    }
+
+    boolean contains(String s) {
+        for (String i : data) {
+            if (i.equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    String remove(int index){
+        while (index++>=nb){
+            this.data[index]=this.data[index+1];
+        }
+        return this.data;
+    }
+
+    String set(int index, String element){
+        return this.data[index]=element;
+    }
+    public boolean isEmpty(){
+        
+        return this.nb == 0;
+    }
+
+    int size(){
+        return this.nb;
     }
 
     public static void main(String[] args) {
         ArrayList alist = new ArrayList();
         System.out.println(alist.toString());
-        alist.ajout("");
+        alist.ajout("test1");
         System.out.println(alist.toString());
-        for (int i = 0; i < 12; i++) {
-            alist.ajout("test" + i);
-        }
+        alist.ajout("test2");
+        alist.ajout("test3");
+        alist.ajout("test4");
+        alist.ajout("test5");
+        alist.ajout("test6");
+        alist.ajout("test7");
+        alist.ajout("test8");
+        alist.ajout("test9");
+        alist.ajout("test10");
+        alist.ajout("test11");
+        alist.ajout("test12");
         System.out.println(alist.toString());
         System.out.println(alist.get(0));
 
