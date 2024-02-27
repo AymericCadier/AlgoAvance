b9de43e8d5a4e62b2e4480b3346add45787797d1
adrianjz02 <adr.jimenez.ian@gmail.com>
2024-02-27 14:09:20 +0100

    modified:   src/main/java/fr/pantheonsorbonne/cri/TreeSet/StringTreeSet.java

--- src/main/java/fr/pantheonsorbonne/cri/TreeSet/StringTreeSet.java
+++ src/main/java/fr/pantheonsorbonne/cri/TreeSet/StringTreeSet.java
@@ -114,6 +114,96 @@ public class StringTreeSet {
         return new StringTreeSetIterator(this.racine);
     }
 
+    public boolean remove(String e) {
+        if (racine == null) {
+            return false; // L'arbre est vide, l'élément n'est pas présent
+        }
+
+        Noeud parent = null;
+        Noeud courant = racine;
+        boolean isLeftChild = false;
+
+        // Recherche de l'élément à supprimer
+        while (courant != null) {
+            int c = e.compareTo(courant.getData());
+            if (c == 0) {
+                break; // Élément trouvé
+            } else {
+                parent = courant;
+                if (c < 0) {
+                    courant = courant.getGauche();
+                    isLeftChild = true;
+                } else {
+                    courant = courant.getDroit();
+                    isLeftChild = false;
+                }
+            }
+        }
+
+        if (courant == null) {
+            return false; // L'élément n'est pas présent dans l'arbre
+        }
+
+        // Cas où le nœud à supprimer n'a pas d'enfants
+        if (courant.getGauche() == null && courant.getDroit() == null) {
+            if (courant == racine) {
+                racine = null;
+            } else if (isLeftChild) {
+                parent.setGauche(null);
+            } else {
+                parent.setDroit(null);
+            }
+        }
+        // Cas où le nœud à supprimer a un seul enfant
+        else if (courant.getDroit() == null) {
+            if (courant == racine) {
+                racine = courant.getGauche();
+            } else if (isLeftChild) {
+                parent.setGauche(courant.getGauche());
+            } else {
+                parent.setDroit(courant.getGauche());
+            }
+        } else if (courant.getGauche() == null) {
+            if (courant == racine) {
+                racine = courant.getDroit();
+            } else if (isLeftChild) {
+                parent.setGauche(courant.getDroit());
+            } else {
+                parent.setDroit(courant.getDroit());
+            }
+        }
+        // Cas où le nœud à supprimer a deux enfants
+        else {
+            Noeud successor = getSuccessor(courant);
+            if (courant == racine) {
+                racine = successor;
+            } else if (isLeftChild) {
+                parent.setGauche(successor);
+            } else {
+                parent.setDroit(successor);
+            }
+            successor.setGauche(courant.getGauche());
+        }
+
+        return true;
+    }
+
+    private Noeud getSuccessor(Noeud node) {
+        Noeud parentOfSuccessor = node;
+        Noeud successor = node;
+        Noeud current = node.getDroit();
+        while (current != null) {
+            parentOfSuccessor = successor;
+            successor = current;
+            current = current.getGauche();
+        }
+        if (successor != node.getDroit()) {
+            parentOfSuccessor.setGauche(successor.getDroit());
+            successor.setDroit(node.getDroit());
+        }
+        return successor;
+    }
+
     public int size() {
         int cpt = 0;
         if (this.racine == null) {
@@ -148,5 +238,7 @@ public class StringTreeSet {
         System.out.println(set.isEmpty());
         System.out.println(set.contains("a"));
         System.out.println(set.first());
+        set.remove("a");
+        System.out.println(set.toString());
     }
 }
